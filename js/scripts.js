d1b30d1d72a194d1eae18a344a326ed15a5df62f
cass1618 <cass16180@gmail.com>
2021-04-04 15:04:50 -0700

    Work in progress: adjust layout and style

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,44 @@ function beepBoop (userNumber) {
   return array;
 }
 
-// UI LOGIC
+function showEach(beepBoopArray) {
+  for (let i = 0; i < beepBoopArray.length; i++) {
+    setTimeout (function() {
+    $(".beep-boop").text(beepBoopArray[i]);
+    }, ((400*i)+((i%2)+1)*300)
+    )    
+    }; 
+  }
+
+  
+
+  function showString(beepBoopString) {
+    $(".fullString").text(beepBoopString).show();
+  }
+
+  function spaceArray(array) {
+    let string = array.join("* *");
+    return string.split("*")
+  }
+
+//UI LOGIC
 $(document).ready(function() {
+  $("#robogers").hover(function() {
+    $("#credit").toggle();
+  });
+
   $("#beep-boop").submit(function(event) {
     event.preventDefault();
 
-    const userNumber = $("#number").val();
-    $(".beep-boop").text(beepBoop(userNumber));
+    
 
-    $("beep-boop").show();
+    
+
+    const userNumber = $("#number").val();
+    const beepBoopArray = beepBoop(userNumber);
+    const beepBoopString = beepBoopArray.join();
+    const beepBoopDisplay = spaceArray(beepBoopArray);        
+    showEach(beepBoopDisplay);
+    showString(beepBoopString);
   });
 });
